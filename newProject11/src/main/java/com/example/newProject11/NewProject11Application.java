d32572170cbf21f3444e5f8f6dad093ddef87531
Sanjay Dinkar <sanjaydinkar2198@gmail.com>
2024-10-27 21:39:40 +0530

    Initial commit

---
+++ newProject11/src/main/java/com/example/newProject11/NewProject11Application.java
@@ -0,0 +1,13 @@
+package com.example.newProject11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProject11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewProject11Application.class, args);
+	}
+
+}
---
+++ newProject11/src/test/java/com/example/newProject11/NewProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newProject11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProject11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
